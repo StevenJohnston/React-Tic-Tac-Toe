08415a7b2d81c9ff439fbf10d50849adec6c74f1
steven <wheatthins@hotmail.ca>
2017-01-10 03:37:47 +0000

    Starter react project - socket io - express

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            React tic tac toe start
+         </div>
+      );
+   }
+}
+
+export default App;
